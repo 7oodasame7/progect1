4b30971c7cfe5bb41a9af9c2664fa58d7e4f97c0
mahmoud-sameh <hooda1331@gmail.com>
2022-01-05 03:04:20 +0200

    finish-project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 let icon = document.getElementById('menu')
 let list = document.querySelector('.list')
 
-icon.onclick = ()=> {
+icon.onclick = () => {
     list.classList.toggle('active')
     icon.classList.toggle('fa-times')
+}
+
+document.onscroll = () => {
+    list.classList.remove('active')
+    icon.classList.remove('fa-times')
+    if (window.scrollY > 300) {
+        document.querySelector('.top').style.display = 'block'
+    } else {
+        document.querySelector('.top').style.display = 'none'
+    }
+}
+
+document.querySelector('.top').onclick =()=> {
+    window.scrollTo({
+        behavior:"smooth",
+        top:0
+    })
 }
